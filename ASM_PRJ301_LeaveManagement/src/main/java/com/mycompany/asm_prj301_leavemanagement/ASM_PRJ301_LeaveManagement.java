e207c4e0c0a9e15746ba8b6b74cbe3c220e34f7d
VuietKie <vuhiep27082005@gmail.com>
2025-07-14 16:15:35 +0700

    Tạo project Maven

---
+++ ASM_PRJ301_LeaveManagement/src/main/java/com/mycompany/asm_prj301_leavemanagement/ASM_PRJ301_LeaveManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.asm_prj301_leavemanagement;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class ASM_PRJ301_LeaveManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
